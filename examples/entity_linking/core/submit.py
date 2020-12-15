4de4c45b4d1ca470930bbe26833304d3fc28af03
wjunnneg <1194348056@qq.com>
2020-12-15 13:57:22 +0800

    update 0.85091.

--- examples/entity_linking/core/submit.py
+++ examples/entity_linking/core/submit.py
@@ -12,13 +12,11 @@ sys.path.append(project_dir)
 os.chdir(sys.path[-1])
 
 import json
-import random
 import pandas as pd
 from tqdm import tqdm
 from argparse import ArgumentParser
 
 from examples.entity_linking.lib.lib_tools import seed_everything
-from examples.entity_linking.lib import lib_tools
 
 seed_everything(seed=42)
 
@@ -34,69 +32,101 @@ class Submit(object):
         self.submit_json = os.path.join(PATH.SUBMIT_DIR, 'result.json')
 
     def main(self):
-        entity_link_list, entity_type_list = [], []
+        entity_link_list, entity_link_prob_list, entity_type_list, entity_type_prob_list = [], [], [], []
 
         with open(self.entity_link_predictions_json_path, 'r', encoding='utf-8') as file:
             for line in tqdm(file):
                 line = json.loads(line)
                 entity_link_list.append(line['label'])
+                entity_link_prob_list.append(line['probs'][line['label']])
 
         with open(self.entity_type_predictions_json_path, 'r', encoding='utf-8') as file:
             for line in tqdm(file):
                 line = json.loads(line)
                 entity_type_list.append(line['label'])
+                entity_type_prob_list.append(line['probs'][line['label']])
 
         entity_link_df = pd.read_csv(filepath_or_buffer=self.entity_link_test_tsv_path, sep='\t')
         entity_link_df = entity_link_df[['text_id', 'entity', 'offset', 'short_text', 'kb_id']]
         entity_link_df['entity_link'] = entity_link_list
+        entity_link_df['entity_link_prob'] = entity_link_prob_list
 
         entity_type_df = pd.read_csv(filepath_or_buffer=self.entity_type_test_tsv_path, sep='\t')
         entity_type_df = entity_type_df[['text_id', 'entity', 'offset', 'short_text', 'type']]
         entity_type_df['entity_type'] = entity_type_list
         entity_type_df['entity_type'] = entity_type_df['entity_type'].map(
             {value: key for (key, value) in CONFIG.PICKLE_DATA['TYPE_TO_IDX'].items()})
+        entity_type_df['entity_type_prob'] = entity_type_prob_list
 
         df = entity_link_df.merge(entity_type_df, on=('text_id', 'offset'), how='outer', suffixes=('', '_et'))
 
         df_ = df.iloc[entity_link_df.shape[0]:]
         df_['entity'] = df_['entity_et']
         df_['short_text'] = df_['short_text_et']
-        df_ = df_[['text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link', 'entity_type']]
         df_['kb_id'] = -1
-        df_['entity_link'] = 0
+        df_['entity_link'] = '0'
+        df_['entity_link_prob'] = '0'
+        df_['entity_link_pair'] = df_['entity_link'] + '_' + df_['entity_link_prob']
+        df_['entity_type_prob'] = df_['entity_type_prob'].apply(lambda a: str(round(a, 3)))
+        df_['entity_type_pair'] = df_['entity_type'] + '_' + df_['entity_type_prob']
+        df_ = df_[['text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link_pair', 'entity_type_pair']]
 
         df = df.loc[:entity_link_df.shape[0] - 1]
-        df = df[['text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link', 'entity_type']]
+        df['entity_link'] = df['entity_link'].astype(str)
+        df['entity_link_prob'] = df['entity_link_prob'].apply(lambda a: str(round(a, 3)))
+        df['entity_link_pair'] = df['entity_link'] + '_' + df['entity_link_prob']
+        df['entity_type_prob'] = df['entity_type_prob'].apply(lambda a: str(round(a, 3)))
+        df['entity_type_pair'] = df['entity_type'] + '_' + df['entity_type_prob']
+        df = df[['text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link_pair', 'entity_type_pair']]
 
-        df = pd.merge(df, df_, how='outer', on=['text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link', 'entity_type'])
+        df = df.merge(df_, how='outer',
+                      on=('text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link_pair', 'entity_type_pair'))
         df['text_id'] = df['text_id'].astype(int)
+
+        df.to_csv('result.csv', encoding='utf-8', index=None)
         df.sort_values(by=['text_id', 'offset'], inplace=True)
         df.reset_index(drop=True, inplace=True)
 
-        df['entity_link'] = df['entity_link'].astype(int)
         df['kb_id'] = df['kb_id'].astype(int)
+        df['entity_link'] = df['entity_link_pair'].apply(lambda a: int(float(a.split('_')[0])))
+        df['entity_link_prob'] = df['entity_link_pair'].apply(lambda a: float(a.split('_')[1]))
+        df['entity_type'] = df['entity_type_pair'].apply(lambda a: a.split('_')[0])
+        df['entity_type_prob'] = df['entity_type_pair'].apply(lambda a: float(a.split('_')[1]))
 
-        sample_list = []
-        for (text_id_int, text_id_df) in df.groupby(by=['text_id']):
-            sample_dict = {}
+        df = df[
+            ['text_id', 'entity', 'offset', 'short_text', 'kb_id', 'entity_link', 'entity_type', 'entity_link_prob',
+             'entity_type_prob']]
+
+        df.drop_duplicates(inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
+        count, sample_list = 0, []
+        for (text_id_int, text_id_df) in tqdm(df.groupby(by=['text_id'])):
+            sample_dict = dict({})
             sample_dict['text_id'] = str(text_id_int)
+            # short_text
             sample_dict['text'] = text_id_df.iloc[0, 3]
             mention_data_list = []
 
             for (offset_int, offset_df) in text_id_df.groupby(by=['offset']):
                 mention_sample_dict = {}
 
-                offset_df.sort_values(by=['entity_link'], ascending=False, inplace=True)
+                offset_df.sort_values(by=['entity_link', 'entity_link_prob'], ascending=False, inplace=True)
 
-                if offset_df.shape[0] >= 3:
+                if sum(offset_df['entity_link'].tolist()) >= 2:
+                    count += 1
                     print(offset_df)
 
-                if int(offset_df.iloc[0, -2]) == 1:
+                # entity_link
+                if int(offset_df.iloc[0, 5]) in [1]:
+                    # kb_id
                     mention_sample_dict['kb_id'] = str(offset_df.iloc[0, 4])
-                elif int(offset_df.iloc[0, -2]) == 0:
-                    mention_sample_dict['kb_id'] = 'NIL_' + str(offset_df.iloc[0, -1])
-                assert int(offset_df.iloc[0, -2]) in [0, 1]
+                elif int(offset_df.iloc[0, 5]) in [0]:
+                    # entity_type
+                    mention_sample_dict['kb_id'] = 'NIL_' + str(offset_df.iloc[0, 6])
+                assert int(offset_df.iloc[0, 5]) in [0, 1]
 
+                # entity
                 mention_sample_dict['mention'] = offset_df.iloc[0, 1]
                 mention_sample_dict['offset'] = str(offset_int)
                 mention_data_list.append(mention_sample_dict)
@@ -104,6 +134,8 @@ class Submit(object):
             sample_dict['mention_data'] = mention_data_list
 
             sample_list.append(sample_dict)
+
+        print('count: {}'.format(count))
 
         os.remove(path=self.submit_json)
 
